d0e8f3703aa5c4c380a0582febb27848a2148909
Austin Curtis <austin.curtis.mail@gmail.com>
2019-10-23 13:11:49 -0600

    Updates

---
+++ precache-manifest.d547d2c34b4f27eb79ac1409884d960a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e213e48d49760e5c515907359e38fd3a",
+    "url": "/react-tutorials/recipe-app-tutorial/build/index.html"
+  },
+  {
+    "revision": "30670902b59f9053c72e",
+    "url": "/react-tutorials/recipe-app-tutorial/build/static/css/main.b438afa5.chunk.css"
+  },
+  {
+    "revision": "1312df30ef6b2a6df726",
+    "url": "/react-tutorials/recipe-app-tutorial/build/static/js/2.c8f85ee2.chunk.js"
+  },
+  {
+    "revision": "30670902b59f9053c72e",
+    "url": "/react-tutorials/recipe-app-tutorial/build/static/js/main.af897c47.chunk.js"
+  },
+  {
+    "revision": "9105ac1260708a7c3d11",
+    "url": "/react-tutorials/recipe-app-tutorial/build/static/js/runtime-main.19704d62.js"
+  }
+]);
